00d73efc6b1fe19d9fc2e5cc568fc71949c40e77
alexdriedger <aldriedger@gmail.com>
2020-04-13 01:16:43 -0700

    Created relics and blights atlas and script

--- scripts/convert_atlas.py
+++ scripts/convert_atlas.py
@@ -1,4 +1,6 @@
-def convert_power_atlas(encoding_start_num: int) -> int: 
+from typing import Dict, Tuple
+
+def convert_power_atlas(encoding_start_num: int) -> Tuple[int, Dict[str, str]]: 
 
   output_file = "out/new_powers.atlas"
   input_file = "powers.atlas"
@@ -30,17 +32,48 @@ def convert_power_atlas(encoding_start_num: int) -> int:
         # Copy info of atlas into new atlas
         else:
           atlas_output_file.write(line)
-
-
-  mapping_file = "out/power_name_map.txt"
-  with open(mapping_file, "w") as map_file:
-    # Tried to use json.dump but it replaced every '\' with '\\'
-    sorted_dict = dict(sorted(mapping.items()))
-    for k, v in sorted_dict.items():
-      map_file.writelines(f"{k} => \"{v}\"\n")
   
   encoding -= 1
-  return encoding
+  return encoding, mapping
+
+def convert_custom_atlas(encoding_start_num: int) -> Tuple[int, Dict[str, str]]:
+  output_file = "out/new_relics_blights.atlas"
+  input_file = "relics_blights.atlas"
+
+  encoding = encoding_start_num
+  mapping = dict()
+
+  with open(output_file, "w") as atlas_output_file:
+    with open(input_file, "r") as atlas_input_file:
+      for index, line in enumerate(atlas_input_file):
+        # Copy info of atlas into new atlas
+        if line.startswith(" ") or index < 6:
+          atlas_output_file.write(line)
+        else:
+          # Get power name from file
+          icon_name = line.strip()
+
+          # Convert code point to a surrate pair (eg. F0000 => \uDB80\uDC0)
+          enc = int_encoding_to_surrogate_pair(encoding)
+
+          # Map name of icon to surrogate pairing so people ctrl+f for icon names
+          # and copy paste encoding into translation file 
+          mapping[icon_name] = enc
+
+          # Encode atlas with new code point for icon
+          atlas_output_file.write(f"{encoding:x}\n")
+
+          encoding += 1
+  
+  encoding -= 1
+  return encoding, mapping
+
+def convert_all_atlases() -> None:
+  start_encoding = int("0xF0000", 16)
+  encoding, mapping_dict = convert_power_atlas(start_encoding)
+  encoding, relics_dict = convert_custom_atlas(encoding)
+  mapping_dict.update(relics_dict)
+  write_mapping_file(mapping_dict)
 
 def int_encoding_to_surrogate_pair(code_point: int) -> str:
   hex_val = chr(code_point)
@@ -51,4 +84,11 @@ def int_encoding_to_surrogate_pair(code_point: int) -> str:
 def pad_hex(padding: int, value: bytes) -> str:
   return f"{value:0{padding}X}"
 
-convert_power_atlas(int("0xF0000", 16))
+def write_mapping_file(mapping: Dict[str, str], file_name: str = "out/custom_emoji_mapping.txt") -> None:
+  with open(file_name, "w") as map_file:
+    # Tried to use json.dump but it replaced every '\' with '\\'
+    sorted_dict = dict(sorted(mapping.items()))
+    for k, v in sorted_dict.items():
+      map_file.writelines(f"{k} => \"{v}\"\n")
+
+convert_all_atlases()
