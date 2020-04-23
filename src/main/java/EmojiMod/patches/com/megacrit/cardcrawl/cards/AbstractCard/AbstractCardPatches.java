bc2efa596409bae88c60fe0b0405b2b44fee4250
alexdriedger <aldriedger@gmail.com>
2020-04-22 17:16:13 -0700

    Fixed line spacing

--- src/main/java/EmojiMod/patches/com/megacrit/cardcrawl/cards/AbstractCard/AbstractCardPatches.java
+++ src/main/java/EmojiMod/patches/com/megacrit/cardcrawl/cards/AbstractCard/AbstractCardPatches.java
@@ -5,6 +5,12 @@ import com.evacipated.cardcrawl.modthespire.lib.ByRef;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import javassist.CannotCompileException;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+import org.lwjgl.Sys;
 
 public class AbstractCardPatches {
 
@@ -29,6 +35,35 @@ public class AbstractCardPatches {
             name[0] = EmojiMod.emojiSupport.FilterEmojis(name[0]);
             rawDescription[0] = EmojiMod.emojiSupport.FilterEmojis(rawDescription[0]);
         }
+    }
+
+    @SpirePatch(clz = AbstractCard.class, method = "renderDescription")
+    public static class LineSpacingPatch {
+        public static boolean firstPatch = true;
+
+        public static ExprEditor Instrument() {
+            return new ExprEditor() {
+                @Override
+                public void edit(MethodCall m) throws CannotCompileException
+                {
+                    if (m.getClassName().equals("com.megacrit.cardcrawl.helpers.FontHelper") &&
+                            m.getMethodName().equals("renderRotatedText")) {
+                        if (firstPatch) {
+                            firstPatch = false;
+                            return;
+                        }
+                        EmojiMod.logger.info("Replacing renderRotatedText");
+                        m.replace(
+                    "{ " +
+                                "$7 = i * 2.50F * -font.getCapHeight() + draw_y - this.current_y + -6.0F;" +
+                                "$_ = $proceed($$);" +
+                            " }"
+                        );
+                    }
+                }
+            };
+        }
+
     }
 
 
