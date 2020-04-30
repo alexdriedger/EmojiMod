30087065952776ba56d74bcf4037d93765fe24eb
alexdriedger <aldriedger@gmail.com>
2020-04-30 00:18:35 -0700

    Replace dynamic variable number with emoji

--- src/main/java/EmojiMod/patches/com/megacrit/cardcrawl/ReplaceDynamicVariableExprEditor.java
+++ src/main/java/EmojiMod/patches/com/megacrit/cardcrawl/ReplaceDynamicVariableExprEditor.java
@@ -5,9 +5,12 @@ import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
+import java.util.stream.Collectors;
+
 public class ReplaceDynamicVariableExprEditor extends ExprEditor {
 
     private int callCount;
+    private static String[] emojiNumberMappings = { "\uDB80\uDE65", "\uDB80\uDE60", "\uDB80\uDE64", "\uDB80\uDE63", "\uDB80\uDE5E", "\uDB80\uDE5D", "\uDB80\uDE62", "\uDB80\uDE61", "\uDB80\uDE5C", "\uDB80\uDE5F" };
 
     public ReplaceDynamicVariableExprEditor() {
         callCount = 0;
@@ -16,13 +19,10 @@ public class ReplaceDynamicVariableExprEditor extends ExprEditor {
     @Override
     public void edit(MethodCall m) throws CannotCompileException {
         if (m.getClassName().equals("java.lang.StringBuilder") && m.getMethodName().equals("append")) {
-            EmojiMod.logger.info("ReplaceDynamicVariable count: " + callCount);
             if (callCount != 3) {
                 callCount++;
                 return;
             }
-
-            EmojiMod.logger.info("Replacing BaseMod RenderDV");
 
             m.replace(
         "{" +
@@ -30,19 +30,21 @@ public class ReplaceDynamicVariableExprEditor extends ExprEditor {
                 "}"
             );
 
-//            m.replace(
-//        "{" +
-//                    "$1 = " + ReplaceDynamicVariableExprEditor.class.getName() + ".replaceIntWithEmoji($1);" +
-//                    "$_ = $proceed($$);" +
-//                "}"
-//            );
-
             callCount++;
         }
 
     }
 
     public static String replaceIntWithEmoji(int num) {
-        return "\uDB80\uDE60 ";
+        String delim = "  ";
+        if (num >= 999) {
+            return "☠️" + delim;
+        }
+        String str = String.valueOf(num)
+                .chars()
+                .map(Character::getNumericValue)
+                .mapToObj(x -> emojiNumberMappings[x])
+                .collect(Collectors.joining(delim, "", delim));
+        return str + delim;
     }
 }
